2e3370a50556fa6a65fc7ab574dc7cc5db3f622c
chrweave <53875531+chrweave@users.noreply.github.com>
2020-05-12 13:23:24 -0500

    made efficient

--- rg64.c
+++ rg64.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define LROT64(x,y) (x<<y)|(x>>(64-y))
-
 typedef unsigned __int128 Uint128;
 typedef unsigned long Uint64;
 typedef unsigned int Uint;
@@ -14,20 +12,6 @@ pa[]={0xba6a677d,0x85e295d2,0x0d152a5f,0xe1d281bd};
 
 Uint128 a=0u,b=0u,c=0u;
 
-void db(Uint128 x){
-	int i;
-	for (i=0;i<128; i++){
-		printf("%u",(x>>(127-i))&1u);
-	}
-	printf("\n");
-}
-void db64(Uint64 x){
-	int i;
-	for (i=0;i<64; i++){
-		printf("%u",(x>>(63-i))&1u);
-	}
-	printf("\n");
-}
 void loadUint128(Uint64 * px, Uint128* x){
 	int i;
 	*x=0u;
@@ -44,31 +28,36 @@ void seed (char * x){
 	}
 	b^=k;
 }
-int main(int argc, char ** argv){
-	int i,j,k;
-	Uint128 q;
-	Uint64 o,*s;
-	if(argc > 1){
-		k=atoi(argv[1]);
-	} else {
-		k=1000;
-	}
+void init(void){
 	loadUint128(pa,&a);
 	loadUint128(pb,&b);
 	loadUint128(pc,&c);
-	s=(Uint64*)malloc(k*64);
+}
+void process(int k){
+	int i,j;
+	
 	for(i=0;i<k;i++){
 		b*=a;
 		b+=c;
 		q=b^(b>>37);
 		o=(Uint64)q;
 		j=59u-(Uint)(b>>123));
-		printf("%u\n",j);
-		o=LROT64(o,j);
-		s[k]=o;
+		o=LROT64(o,j)(o<<j)|(o>>(64-j));
 		printf("%016llX\n",o);
 	}
-	db64(o);
-	free(s);
+}
+int main(int argc, char ** argv){
+	int i,j,k;
+	Uint128 q;
+	Uint64 o;
+	if(argc > 1){
+		k=atoi(argv[1]);
+		if(argc > 2){
+			seed(argv[2]);
+		}
+	} else {
+		k=1000;
+	}
+	process
 	return 0;
 }
