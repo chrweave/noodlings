f37df6945542a14b6cc82ab3b6aff83d1cbabef6
Chris Weaver <cweaver@drelm.knology.net>
2020-06-25 15:37:40 -0500

    kws202006251537

--- kws.c
+++ kws.c
@@ -10,13 +10,32 @@ struct dll{
 
 typedef struct dll Dll;
 
+struct ll {
+    void* data;
+    struct ll* next;
+};
+
+typedef struct ll Ll;
 
 char repo[4096];
+Dll* table[65536];
+Dll* alphaTree;
 
+void initTable(void){
+    int i = 0;
+    for(i=0;i<65536;i++){
+        table[i]=(Dll*) malloc(sizeof(Dll));
+    }
+}
+
+void setString(Dll* d, char * in){
+    d->data=malloc(strlen(in)+1);
+    strcpy((char*)d->data,in);
+}
 
 void parseArgs(int argc, char ** argv){
     int i = 0;
-    for (i=0;i<argc,i++){
+    for (i=0;i<argc;i++){
         if(strcmp(argv[i],"-w")==0){
             strcpy(repo,argv[i+1]);
             i++;
