3ac8d928bc5c7a69bd8d1b8af81ca1223d489cd9
chrweave <53875531+chrweave@users.noreply.github.com>
2020-08-25 08:54:51 -0500

    pretty

--- lch.c
+++ lch.c
@@ -4,10 +4,10 @@ typedef unsigned long long V;
 
 int main (int argc, char ** argv){
     FILE* f;
-V a=0xE328D93B7C7102DDu;
-V c=0xC96D90E9C729D203u;
-V b;
-V h;
+    V a=0xE328D93B7C7102DDu;
+    V c=0xC96D90E9C729D203u;
+    V b;
+    V h;
     if(argc>1 && (f=fopen(argv[1],"r"))!=NULL){
         char buff [1024];
         char * q;
@@ -20,8 +20,8 @@ V h;
             h=c;
             while(b=(V)(*q++)){
                 h ^= b;
-                h*=a;
-                h+=c;
+                h *= a;
+                h += c;
             }
             printf("%016llX %s\n",h,buff);
         }while(1);
