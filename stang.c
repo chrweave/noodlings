ee3b26f39c77bc23ee0a672db3eb2c594bbbac60
Chris Weaver <chrweave@gmail.com>
2020-10-05 23:07:54 -0500

    stang2

--- stang.c
+++ stang.c
@@ -1,6 +1,27 @@
 #include<stsio.h>
 #include<stdlib.h>
 
+int numSamples;
+int numNodes;
+double * positions;
+double * deltas;
+double * momentums;
+double bridge;
+int bridgeSize;
+
+void parseArguments(char ** argv){
+    numSamples=atoi(argv[1]);
+    numNodes=atoi(argv[2]);
+    bridgeSize=atoi(argv[3]);
+    positions=(double*)malloc(sizeof(double)*numNodes);
+    deltas=(double*)malloc(sizeof(double)*numNodes);
+    momentums=(double*)malloc(sizeof(double)*numNodes);
+}
+void setInitialConditions(){
+}
 int main(int argc, char ** argv){
+    if(argc > 3 ){
+        parseArguments(argv);
+    }
     return 0;
 }
