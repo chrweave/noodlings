cf840abd867f5de858f4e963fe3fbbb6b9efa6f2
Chris Weaver <chrweave@gmail.com>
2020-10-06 09:02:29 -0500

    pxstanger2

--- psx.m
+++ psx.m
@@ -1,12 +1,13 @@
 function y=psx(windowSize,stretchFactor,sourceVector)
+%function y=psx(windowSize,stretchFactor,sourceVector)
 
 w=zeros(windowSize,1);
 ww=floor(windowSize/2);
 if mod(w,2)==0,
     w(1:ww)=(0:ww-1)'/(ww-1);
 else
-    w(1:ww)=(0:ww-1)'/(ww); 
-    w(ww)=1/2;
+    %w(1:ww)=(0:ww-1)'/(ww); 
+    %w(ww+1)=1/2;
 end
 w+=flipud(w);
 y=w;
