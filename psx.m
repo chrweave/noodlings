00c04e86efc3e6f565af66234f548a2c44641118
chrweave <53875531+chrweave@users.noreply.github.com>
2020-10-07 16:03:42 -0500

    getting win

--- psx.m
+++ psx.m
@@ -3,10 +3,18 @@ function y=psx(windowSize,stretchFactor,sourceVector)
 
 %w=zeros(windowSize,1);
 ww=windowSize/2;
+www=[floor(ww),windowSize-floor(ww)];
 
 w=ivize(abs(ww-.5-(0:windowSize-1)'));
 
 w.*=w;
 w=1-w;
 w=w.^1.25;
-y=w;
+n=floor(length(sourceVector)/windowSize);
+l=n*windowSize;
+
+y=zeros(l*stretchFactor,1);
+for i=1:n,
+
+end
+
