9e717591f0118802a98e8e051817333c237e7d63
chrweave <53875531+chrweave@users.noreply.github.com>
2020-10-08 13:19:37 -0500

    fixOverlap

--- psx.m
+++ psx.m
@@ -13,8 +13,30 @@ w=w.^1.25;
 n=floor(length(sourceVector)/windowSize);
 l=n*windowSize;
 
-y=zeros(l*stretchFactor,1);
-for i=1:n,
-
+z=zeros(l*stretchFactor,1);
+pa=0;
+ta=0;
+ki=0;
+kj=0;
+q=zeros(windowSize,1);
+for cont=1:(2*n-1),
+    q=w.*sourceVector(pa+1:pa+windowSize);
+    pa+=www(ki+1);
+    ki+=1;
+    ki=mod(ki,2);
+    r=fft(q);
+    sa=rand(windowSize,1);
+    sb=2*pi*i*sa;
+    sc=exp(sb);
+    sd=sc.*r;
+    se=ifft(sd);
+    s=real(se);
+    for j=1:stretchFactor,
+        z(ta+1:ta+windowSize)+=s;
+        ta+=www(kj+1);
+        kj+=1;
+        kj=mod(kj,2);
+    end;
 end
+y=rnorm(z);
 
