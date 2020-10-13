2156a309a2db9f50e7780e0c376ffa62e1bd836f
chrweave <53875531+chrweave@users.noreply.github.com>
2020-10-13 15:49:25 -0500

    fixed phase

--- psx.m
+++ psx.m
@@ -4,12 +4,13 @@ function y=psx(stepSize,stretchFactor,sourceVector)
 %w=zeros(windowSize,1);
 bigStep=stretchFactor*stepSize;
 windowSize=bigStep*2;
-w=ivize(abs(bigStep-.5-(0:windowSize-1)'));
-
-
-w.*=w;
-w=1-w;
-w=w.^1.25;
+w=hann(windowSize);
+%w=ivize(abs(bigStep-.5-(0:windowSize-1)'));
+%
+%
+%w.*=w;
+%w=1-w;
+%w=w.^1.25;
 n=floor((length(sourceVector)-windowSize)/stepSize);
 l=n*bigStep+windowSize;
 
@@ -21,16 +22,15 @@ for cont=1:n,
     q=w.*sourceVector(pa+1:pa+windowSize);
     pa+=stepSize;
     r=fft(q);
-    sa=rand(windowSize,1);
-    sb=2*pi*i*sa;
-    sc=exp(sb);
-    sd=sc.*r;
-    se=ifft(sd);
-    s=real(se).*w;
     for j=1:stretchFactor,
+        sa=rand(windowSize,1);
+        sb=2*pi*i*sa;
+        sc=exp(sb);
+        sd=sc.*r;
+        se=ifft(sd);
+        s=real(se).*w;
         z(ta+1:ta+windowSize)+=s;
         ta+=stepSize;
     end;
 end
 y=rnorm(z);
-
