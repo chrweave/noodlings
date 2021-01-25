2444b0502c246db1d7ce386ec3b3474115ca4b45
chrweave <53875531+chrweave@users.noreply.github.com>
2021-01-25 13:05:42 -0600

    teststring

--- stang.c
+++ stang.c
@@ -3,28 +3,54 @@
 
 int numSamples;
 int numNodes;
+int bridgeSize;
+double * samples;
 double * positions;
 double * deltas;
 double * momentums;
-double bridge;
-int bridgeSize;
+double * bridge;
+
+void clearArray(void* ar){
+    if(ar!=NULL){
+        free(ar);
+    }
+}
+
+double * getDoubleArray(int size){
+    return (double*) malloc (sizeof(double)*size);
+}
+
+void setString(void){
+    int i = 0;
+    for(i=0;i<=numNodes/2;i++){
+        positions[i]=i;
+        positions[numNodes-1-i]=i;
+    }
+    for(i=0;i<numNodes;i++){
+        printf("%d ",i);
+    }
+    printf("\n");
+}
 
 void cleanup(void){
-    if(positions && deltas && momentums){
-        free(positions);
-        free(deltas);
-        free(momentums);
-    }
+    clearArray(positions);
+    clearArray(deltas);
+    clearArray(momentums);
+    clearArray(bridge);
 }
 void parseArguments(char ** argv){
     numSamples=atoi(argv[1]);
     numNodes=atoi(argv[2]);
-    bridgeSize=atoi(argv[3]);
-    positions=(double*)malloc(sizeof(double)*numNodes);
-    deltas=(double*)malloc(sizeof(double)*numNodes);
-    momentums=(double*)malloc(sizeof(double)*numNodes);
+    bridgeSize=atoi(argv[3]);   
 }
 void setInitialConditions(){
+    int i;
+    
+    positions=getDoubleArray(numNodes);
+    deltas=getDoubleArray(numNodes);
+    momentums=getDoubleArray(numNodes);
+    bridge=getDoubleArray(numNodes);
+    setString();
 }
 int main(int argc, char ** argv){
     if(argc > 3 ){
