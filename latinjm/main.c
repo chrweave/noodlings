5c6d5ae20fccba8586d56f8b0c6026a593c9019b
Chris Weaver <chrweave@gmail.com>
2021-02-15 09:21:34 -0600

    latinjm0002

--- latinjm/main.c
+++ latinjm/main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-    printf("Hello World!\n");
+int rc4ArrarySize = 0;
+int *rc4Array;
+
+void parseArgs(char ** argv)
+
+int main(int argc, char ** argv){
+    if(argc > 2){
+        parseArgs(argv);
+    }
     return 0;
 }
