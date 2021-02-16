5dbd54ef62d494daaabea2171e725c6481c3df8d
Chris Weaver <chrweave@gmail.com>
2021-02-15 19:53:16 -0600

    latinjm0009

--- latinjm/main.c
+++ latinjm/main.c
@@ -1,4 +1,5 @@
-/* C version of Pual Hankin's implementation of Jacobson and Matthews random latin square using an rc4 generator */
+/* C version of Pual Hankin's implementation of Jacobson and Matthews random latin square using an rc4 generator
+ * See his original source at: https://blog.paulhankin.net/latinsquares/ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
@@ -96,7 +97,6 @@ int pump(void){
     temp+=rc4Array[rc4x];
     temp%=rc4ArrarySize;
     temp=rc4Array[temp];
-    temp%=cubeDim;
     rc4x++;
     rc4x%=rc4ArrarySize;
     return temp;
@@ -122,44 +122,7 @@ void initRc4Array(char * p){
     }
 }
 
-int getXp(int y,int z){
-    int ret = -2;
-    int i = 0;
-    int c = 1;
-    for(i=0;c && i<cubeDim;i++){
-        if(cube[i][y][z]){
-            ret=i;
-            c=0;
-        }
-    }
-    return ret;
-}
 
-int getYp(int x,int z){
-    int ret = -2;
-    int i = 0;
-    int c = 1;
-    for(i=0;c && i<cubeDim;i++){
-        if(cube[x][i][z]){
-            ret=i;
-            c=0;
-        }
-    }
-    return ret;
-}
-
-int getZp(int x,int y){
-    int ret = -2;
-    int i = 0;
-    int c = 1;
-    for(i=0;c && i<cubeDim;i++){
-        if(cube[x][y][i]){
-            ret=i;
-            c=0;
-        }
-    }
-    return ret;
-}
 
 void clearSingleVolatile(void* v){
     if(v!=NULL){
@@ -173,44 +136,12 @@ void clearVolatiles(void){
     clearSingleVolatile((void*)rc4Array);
 }
 
-void getPrimes(CubePoint * a, CubePoint * b){
-    int c;
-    b->x=getXp(a->y,a->z);
-    b->y=getYp(a->x,a->z);
-    b->z=getZp(a->x,a->y);
-    c= (b->x>=0)&&(b->y>=0)&&(b->z>=0);
-    if(!c){
-        printf("%d %d %d were found as primes.\n",b->x,b->y,b->z);
-        clearVolatiles();
-        exit(EXIT_FAILURE);
-    }
-}
+
 
 void randomPoint(CubePoint * a){
-    a->x=pump();
-    a->y=pump();
-    a->z=pump();
-}
-
-void warpCube(void){
-    int i =0;
-    int l=cubeDim*cubeDim;
-    CubePoint rando;
-    CubePoint prime;
-    CubePoint neg;
-    int perfect = 1;
-
-    randomPoint(&rando);
-    for(i=0;i<l;i++){
-        if(perfect){
-            while(cube[rando.x][rando.y][rando.z]==1){
-                randomPoint(&rando);
-            }
-            getPrimes(&rando,&prime);
-        } else {
-            getPrimes(&neg,&prime);
-        }
-    }
+    a->x=pump()%cubeDim;
+    a->y=pump()%cubeDim;
+    a->z=pump()%cubeDim;
 }
 
 void initMarginals(void){
@@ -230,11 +161,88 @@ void initMarginals(void){
     }
 }
 
+void rand2(int a, int b, int * c, int *d ) {
+    if (pump()%2 == 0) {
+        *c=a;
+        *d=b;
+    } else {
+        *c=b;
+        *d=a;
+    }
+}
+
+void latin(void) {
+    int mxy, mxz, myz;
+    int m [3];
+    int proper = 1;
+    int minIter = cubeDim*cubeDim*cubeDim;
+    int iter;
+
+    initMarginals();
+    for(iter=0;iter<minIter||!proper; iter++){
+        int i, j, k, i2, j2, k2, i2_, j2_, k2_;
+
+        if (proper) {
+            // Pick a random 0 in the array
+            do{
+                i = pump()%cubeDim;
+                j = pump()%cubeDim;
+                k = pump()%cubeDim;
+            }while( xy[i][j] == k);
+            // find i2 such that [i2, j, k] is 1. same for j2, k2
+            i2 = yz[j][k];
+            j2 = xz[i][k];
+            k2 = xy[i][j];
+            i2_=i;
+            j2_=j;
+            k2_=k;
+        } else {
+            i = m[0];
+            j = m[1];
+            k = m[2];
+            // find one such that [i2, j, k] is 1, same for j2, k2.
+            // each is either the value stored in the corresponding
+            // slice, or one of our three temporary "extra" 1s.
+            // That's because (i, j, k) is -1.
+            rand2(yz[j][k], myz,&i2,&i2_);
+            rand2(xz[i][k], mxz,&j2,&j2_);
+            rand2(xy[i][j], mxy,&k2,&k2_);
+        }
+
+        proper = (xy[i2][j2] == k2);
+        if (!proper) {
+            m[0]=i2;
+            m[1]=j2;
+            m[2]=k2;
+            mxy = xy[i2][j2];
+            myz = yz[j2][k2];
+            mxz = xz[i2][k2];
+        }
+
+        xy[i][j] = k2_;
+        xy[i][j2] = k2;
+        xy[i2][j] = k2;
+        xy[i2][j2] = k;
+
+        yz[j][k] = i2_;
+        yz[j][k2] = i2;
+        yz[j2][k] = i2;
+        yz[j2][k2] = i;
+
+        xz[i][k] = j2_;
+        xz[i][k2] = j2;
+        xz[i2][k] = j2;
+        xz[i2][k2] = j;
+    }
+    return xy;
+}
+
+
 void parseArgs(char ** argv){
     cubeDim=atoi(argv[1]);
     initRc4Array(argv[2]);
-    initCube();
-    initMarginals();
+    //initCube();
+    //initMarginals();
     readCube();
     clearVolatiles();
 }
