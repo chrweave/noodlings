221f1737c34a45ab3958009b32a6d69cd75ce6b4
chrweave <53875531+chrweave@users.noreply.github.com>
2021-03-02 16:04:54 -0600

    sanitize0005

--- wordMaker.js
+++ wordMaker.js
@@ -11,11 +11,6 @@ class wordMaker {
 	rl;
 	bfgkp;
 	seen;
-	lim;
-	lin;
-	lfn;
-	lvs;
-	lws;
 	vowlGen;
 	finGen;
 	iniGen;
@@ -26,62 +21,80 @@ class wordMaker {
 
 	constructor(inWurd, inFilterFlag, inWss){
 		var i;
+		var g="azjvcxoqjkoynrzlwpdsbdkmjhatyooqjidfcghumxlidflwfvzeotyqueivblcdypaluqoiatnxgoefhrknzeqtlkgxephrrglykbgbgldepmrhjsbgqqdffzqbewsdifvuhxtycypebvlvwreuvejoibdgqilqzejrum";
+		var r=new rc4Gen(260);
+		r.initBuffer("Sanitize");		
+		this.filter=String.fromCharCode.apply(null,g.split('').map(x => (x.charCodeAt(0)-71-r.pump()%26)%26+97)).replace(/y/g,'.').split('z').map(y => new RegExp(y));
 		this.a = "badafagahajakalamanaparasatav".split('a'); 
 		this.initial = "bablabradadrafaflafragaglagrahajakaklakralamanapaplaprarasaskaslasmasnaspastatatrav".split('a');
 		this.myfinal = "badafaftagajakalalbaldalfalgaljalkalmalpaltalvamampamtanandankantapaptararbardarfargarjarkarmarnarpartarvaskastatav".split('a');
 		this.vowels = "abebibobu".split('b');
 		this.wss = inWss;
-		this.intern = [];
-		this.plnmt = new Set(['p','l','n','m','t']);
-		this.bdfgkpt = new Set(['b','d','f','g','k','p','t']);
-		this.rl = new Set(['r','l']);
-		this.bfgkp = new Set(['b','f','g','k','p']);
-		this.seen = new Set([]);
-		this.lim = this.intern.length;
-		this.lin = this.initial.length;
-		this.lfn = this.myfinal.length;
-		this.lvs = this.vowels.length;
-		this.lws = this.wss.length;
-		this.vowlGen=new rc4Gen(this.getRgBound(this.lvs));
-		this.finGen=new rc4Gen(this.getRgBound(this.lfn+3));
-		this.iniGen=new rc4Gen(this.getRgBound(this.lin+1));
-		this.ternGen=new rc4Gen(this.getRgBound(this.lim));
-		this.wssGen=new rc4Gen(this.getRgBound(this.lws));
-		this.vowlGen.initBuffer(inWurd);
-		this.finGen.initBuffer(inWurd);
-		this.iniGen.initBuffer(inWurd);
-		this.ternGen.initBuffer(inWurd);
-		this.wssGen.initBuffer(inWurd);
+		this.intern = [
+				"babdabfabgabjabkablabmabnabrabsabtabvabdrabflabfrabglabgrabklabk",
+				"rabslabsmabsnabspabstabtradbadbladbradadfadgadjadkadladmadnadpad",
+				"radsadvadfladfradgladgradkladkradpladpradsladsmadsnadspadstafbaf",
+				"blafbrafdafdrafafgafjafkaflafmafnafpafrafsaftafglafgrafklafkrafp",
+				"lafprafslafsmafsnafspafstaftragbagblagbragdagdragfagflagfragagja",
+				"glagmagnagpagragsagtagvagplagpragslagsmagsnagspagstagtrahajbajbl",
+				"ajbrajdajdrajfajflajfrajgajglajgrajajkajlajmajnajpajrajsajtajvaj",
+				"klajkrajplajprajslajsmajsnajspajstajtrakbakblakbrakdakdrakfakfla",
+				"kfrakjakaklakmaknakpakraksaktakvakplakprakslaksmaksnakspakstaktr",
+				"albalbdalbfalbgalbjalbkalblalbmalbnalbralbsalbtalbvaldbaldaldfal",
+				"dgaldjaldkaldlaldmaldnaldpaldraldsaldvalfbalfdalfalfgalfjalfkalf",
+				"lalfmalfnalfpalfralfsalftalgbalgdalgfalgalgjalglalgmalgnalgpalgr",
+				"algsalgtalgvaljbaljdaljfaljgaljaljkaljlaljmaljnaljpaljraljsaljta",
+				"ljvalkbalkdalkfalkjalkalklalkmalknalkpalkralksalktalkvalalmalnal",
+				"palsaltalvalmbalmdalmfalmgalmjalmkalmlalmnalmpalmralmsalmtalmval",
+				"nbalndalnfalngalnjalnkalnlalnmalnpalnralnsalntalnvalpdalpfalpgal",
+				"pjalpkalplalpmalpnalpralpsalptalpvalsbalsdalsfalsgalsjalskalslal",
+				"smalsnalspalsralstalsvaltbaltfaltgaltjaltkaltlaltmaltnaltpaltral",
+				"tsaltvalvbalvdalvgalvjalvkalvlalvmalvnalvpalvralvsalvtambamblamb",
+				"ramdamdramfamflamframgamglamgramjamkamklamkramlamamnampamramsamt",
+				"amvamplampramslamsmamsnamspamstamtranbanblanbrandandranfanflanfr",
+				"anganglangranjankanklankranlanmananpanransantanvanplanpranslansm",
+				"ansnanspanstantrapdapdrapfapflapfrapgapglapgrapjapkapklapkraplap",
+				"mapnapaprapsaptapvapslapsmapsnapspapstaptrarbarbdarbfarbgarbjarb",
+				"karblarbmarbnarbrarbsarbtarbvardbardardfardgardjardkardlardmardn",
+				"ardpardrardsardvarfbarfdarfarfgarfjarfkarflarfmarfnarfparfrarfsa",
+				"rftargbargdargfargargjarglargmargnargpargrargsargtargvarjbarjdar",
+				"jfarjgarjarjkarjlarjmarjnarjparjrarjsarjtarjvarkbarkdarkfarkjark",
+				"arklarkmarknarkparkrarksarktarkvarmbarmdarmfarmgarmjarmkarmlarma",
+				"rmnarmparmrarmsarmtarmvarnbarndarnfarngarnjarnkarnlarnmarnarnpar",
+				"nrarnsarntarnvarpdarpfarpgarpjarpkarplarpmarpnarparprarpsarptarp",
+				"vararsartarvarsbarsdarsfarsgarsjarskarslarsmarsnarsparsrarstarsv",
+				"artbartfartgartjartkartlartmartnartpartrartsartvarvbarvdarvgarvj",
+				"arvkarvlarvmarvnarvparvrarvsarvtasbasblasbrasdasdrasfasflasfrasg",
+				"asglasgrasjaskasklaskraslasmasnasplaspasprasrasastasvastratbatbl",
+				"atbratfatflatfratgatglatgratjatkatklatkratlatmatnatplatpatpratra",
+				"tslatsmatsnatspatsatstatatvavbavblavbravdavdravgavglavgravjavkav",
+				"klavkravlavmavnavplavpavpravravslavsmavsnavspavsavstavtravtav"
+				].join('').split('a');
+		this.vowlGen=this.setRg(this.vowels.length,inWurd);
+		this.finGen=this.setRg(this.myfinal.length+3,inWurd);
+		this.iniGen=this.setRg(this.initial.length+1,inWurd);
+		this.ternGen=this.setRg(this.intern.length,inWurd);
+		this.wssGen=this.setRg(this.wss.length,inWurd);
 		this.filterFlag=inFilterFlag;
-		for(i=0;i<this.a.length;i++){
-			var j;
-			for(j=0;j<this.a.length;j++){
-				var k;
-				for(k=0;k<this.a.length;k++){
-					this.processIndices(i,j,k);
-				}
-			}
-		}
-		var g="azjvcxoqjkoynrzlwpdsbdkmjhatyooqjidfcghumxlidflwfvzeotyqueivblcdypaluqoiatnxgoefhrknzeqtlkgxephrrglykbgbgldepmrhjsbgqqdffzqbewsdifvuhxtycypeb";
-		var r=new rc4Gen(260);
-		r.initBuffer("Sanitize");		
-		this.filter=String.fromCharCode.apply(null,g.split('').map(x => (x.charCodeAt(0)-71-r.pump()%26)%26+97)).replace(/y/g,'.').split('z').map(y => new RegExp(y));
 	}
 	
 	makeWords(e){
 		return e.map(x => this.makeSingleWord(x));
 	}
 	
-	getRgBound(e){
-		var i=512;
-		if (e>i){
-			i=e;
+	setRg(e,inWurd){
+		var l=512;
+		var rg;
+		if (e>l){
+			l=e;
 		} else {			
-			while(i%e>0){
-				i--;
+			while(l%e>0){
+				l--;
 			}
 		}
-		return i;
+		rg= new rc4Gen(l);
+		rg.initBuffer(inWurd);
+		return rg;
 	}
 	
 	forbidden(q){
@@ -104,98 +117,29 @@ class wordMaker {
 		var t;
 		
 		t=this.wssGen.pump();
-		t%=this.lws;
+		t%=this.wss.length;
 		ws = this.wss[t];
 		dx=this.iniGen.pump();
-		dx%=(this.lin+1);
-		if(dx<this.lin){
+		dx%=(this.initial.length+1);
+		if(dx<this.initial.length){
 			magicWord += this.initial[dx];
 		}
 		for(i=0;i<ws;i++){
 			t=this.vowlGen.pump();
-			t %= this.lvs;
+			t %= this.vowels.length;
 			magicWord += this.vowels[t];
 			t = this.ternGen.pump();
 			magicWord += this.intern[t];
 		}
 		t=this.vowlGen.pump();
-		t %= this.lvs;
+		t %= this.vowels.length;
 		magicWord += this.vowels[t];
 		dx = this.finGen.pump();
-		dx %= (this.lfn+3);
-		if(dx<this.lfn){
+		dx %= (this.myfinal.length+3);
+		if(dx<this.myfinal.length){
 			magicWord += this.myfinal[dx];
 		}
 		return magicWord;
 	}
-	
-	processIndices(i,j,k){
-		var indices = [i, j, k];
-		var myString;
-		if (this.tripleInspectable(indices)){
-			if (this.isNotForbiddenAllPairs(indices)){
-				if (i==j){
-					if(j==k){
-							myString=this.a[i];
-					} else {
-						myString=this.a[i]+this.a[k];
-					}  
-				} else if (j==k) {
-					myString=this.a[i]+this.a[k];
-				} else {
-					myString=this.a[i]+this.a[j]+this.a[k];
-				}
-				if(!this.seen.has(myString)){
-					this.seen.add(myString);
-					this.intern.push(myString);
-				} 
-			}  
-		}
-		return true;
-	}
-	
-	isNotForbiddenAllPairs(indices){
-		var ret = true;
-		var pairs = ["td","vf","pb","gk","rl"];
-		var i;
-		for(i=0;i<pairs.length;i++){    
-			var con = pairs[i].split('');
-			ret = ret && this.isAllowedPair(indices,con);
-		}
-		ret = ret && this.a[indices[0]] != 'h';
-		ret = ret && this.a[indices[1]] != 'h';
-		ret = ret && this.a[indices[2]] != 'h';
-		ret = ret || (indices[0] == indices[1] && indices[1] == indices[2])
-		return ret;
-	}
-	
-	isAllowedPair(indices, con){
-		var q;
-		var ret = true;
-		
-		for(q=0;q<2;q++){
-			var r;
-			for(r=0;r<2;r++){
-				if(this.a[indices[q]] == con[r]){
-					if(this.a[indices[q+1]] == con[1-r]){
-						ret = false;
-					}
-				}
-			}
-		}    
-		return ret;
-	}
-	
-	tripleInspectable(indices){
-		var ret = false;
-			
-		ret = ret || (this.a[indices[1]]=='s' && this.plnmt.has(this.a[indices[2]]));
-		ret = ret || this.rl.has(this.a[indices[0]]);
-		ret = ret || (this.a[indices[2]]=='r' && this.bdfgkpt.has(this.a[indices[1]]));
-		ret = ret || (this.a[indices[2]]=='l' && this.bfgkp.has(this.a[indices[1]]));
-		ret = ret || indices[0] == indices[1];  
-		ret = ret || indices[1] == indices[2];
-		return ret;
-	}
-	
 }
+
