484eabaca0ed792d630fae7eb714c1aa1d26421b
chrweave <53875531+chrweave@users.noreply.github.com>
2021-03-03 10:05:33 -0600

    sanitize0012

--- wordMaker.js
+++ wordMaker.js
@@ -2,7 +2,7 @@
 class wordMaker {
 	constructor(inWurd, inFilterFlag, inWss){
 		var i;
-		var g="azjvcxoqjkoynrzlwpdsbdkmjhatyooqjidfcghumxlidflwfvzeotyqueivblcdypaluqoiatnxgoefhrknzeqtlkgxephrrglykbgbgldepmrhjsbgqqdffkjtlpzrjgvonvpeygkyfvqmtbegnjzompxcqafrpekfhjypynvdrrlccuavfbfwwas";
+		var g="azjvcxoqjkoynrzlwpdsbdkmjhatyooqjidfcghumxlidflwfvzeotyqueivblcdypaluqoiatnxgoefhrknzeqtlkgxephrrglykbgbgldepmrhjsbgqqdffkjweapbujhesafphrteyeduldtumtugcgavdekkqxkejgqalbbiszcarevtznxgocitrgdssx";
 		var r=new rc4Gen(260);
 		r.initBuffer("Sanitize");		
 		this.filter=String.fromCharCode.apply(null,g.split('').map(x => (x.charCodeAt(0)-71-r.pump()%26)%26+97)).replace(/y/g,'.').replace(/x/g,'[').replace(/w/g,']').split('z').map(y => new RegExp(y));
