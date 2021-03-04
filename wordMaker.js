cf9d91761f85e5704932552908ee6e8bfb859ad6
chrweave <53875531+chrweave@users.noreply.github.com>
2021-03-04 14:20:08 -0600

    improved pragmatics

--- wordMaker.js
+++ wordMaker.js
@@ -1,9 +1,46 @@
 //#include <rc4Gen.js>
+class partMaker {
+	constructor(inWurd,inArray,delim,modPlus){
+		var p=modPlus;
+		if (delim ==0){
+			this.palette=inArray;
+			p=0;
+		} else {
+			this.palette=inArray.split(delim);
+		}
+		var l=this.palette.length;
+		this.lim=[l,l+p];
+		this.gen=this.setRg(l+p,inWurd);
+	}
+	getPart(){
+		var t=this.gen.pump()%this.lim[1];
+		var u;
+		if (t<this.lim[0]){
+			u=this.palette[t];
+		} else {
+			u="";
+		}
+		return u;
+	}
+	setRg(e,inWurd){
+		var l=512;
+		if (e>l){
+			l=e;
+		} else {			
+			while(l%e>0){
+				l--;
+			}
+		}
+		var rg= new rc4Gen(l);
+		rg.initBuffer(inWurd);
+		return rg;
+	}	
+}
 class wordMaker {
-	constructor(inWurd, inFilterFlag, inWss){
-		var g = "azjvcxoqjkoynrzlwpdsbdkmjhatyooqjidfcghumxlidflwfvzeotyqueivblcdypaluqoiatnxgoefhrknzeqtlkgxephrrglykbgbgldepmrhjsbgqqdffkjweapb"+
-				"ujhesafphrteyeduldtumtugcgavdekkqxkejgqalbbiszcarevtznxgocitrgdssxoxcmjwjxtdqdudowwgipkk";
-		var h= "babdabfabgabjabkablabmabnabrabsabtabvabdrabflabfrabglabgrabklabkrabslabsmabsnabspabstabtradbadbladbradadfadgadjadkadladmadnadpad"+
+	constructor(inWurd, inWss){
+		var a =	"azjvcxoqjkoynrzlwpdsbdkmjhatyooqjidfcghumxlidflwfvzeotyqueivblcdypaluqoiatnxgoefhrknzeqtlkgxephrrglykbgbgldepmrhjsbgqqdffkjweapb"+
+				"ujhesafphrteyeduldtumtugcgavdekkqxkejgqalbbiszcarevtznxgocitrgdssxoxcmjwjxtdqdudowwgipkkeumjvkfxei";
+		var b=	"babdabfabgabjabkablabmabnabrabsabtabvabdrabflabfrabglabgrabklabkrabslabsmabsnabspabstabtradbadbladbradadfadgadjadkadladmadnadpad"+
 				"radsadvadfladfradgladgradkladkradpladpradsladsmadsnadspadstafbafblafbrafdafdrafafgafjafkaflafmafnafpafrafsaftafglafgrafklafkrafp"+
 				"lafprafslafsmafsnafspafstaftragbagblagbragdagdragfagflagfragagjaglagmagnagpagragsagtagvagplagpragslagsmagsnagspagstagtrahajbajbl"+
 				"ajbrajdajdrajfajflajfrajgajglajgrajajkajlajmajnajpajrajsajtajvajklajkrajplajprajslajsmajsnajspajstajtrakbakblakbrakdakdrakfakfla"+
@@ -22,70 +59,43 @@ class wordMaker {
 				"artbartfartgartjartkartlartmartnartpartrartsartvarvbarvdarvgarvjarvkarvlarvmarvnarvparvrarvsarvtasbasblasbrasdasdrasfasflasfrasg"+
 				"asglasgrasjaskasklaskraslasmasnasplaspasprasrasastasvastratbatblatbratfatflatfratgatglatgratjatkatklatkratlatmatnatplatpatpratra"+
 				"tslatsmatsnatspatsatstatatvavbavblavbravdavdravgavglavgravjavkavklavkravlavmavnavplavpavpravravslavsmavsnavspavsavstavtravtav";
-		var r=new rc4Gen(260);
+		var c = "bablabradadrafaflafragaglagrahajakaklakralamanapaplaprarasaskaslasmasnaspastatatrav";
+		var d = "badafaftagajakalalbaldalfalgaljalkalmalpaltalvamampamtanandankantapaptararbardarfargarjarkarmarnarpartarvaskastatav";
+		var e = "abebibobu";
+		var f = "badafagajakalamanaparasatav";
+		var r = new rc4Gen(260);
+		this.wss = new partMaker(inWurd,inWss,0,0);
+		this.intern     = new partMaker(inWurd,b,'a',0);
+		this.initials   = new partMaker(inWurd,c,'a',1);
+		this.finals     = new partMaker(inWurd,d,'a',3);
+		this.vowels     = new partMaker(inWurd,e,'b',0);
+		this.pragmatics = new partMaker(inWurd,f,'a',0);
 		r.initBuffer("Sanitize");		
-		this.filter=String.fromCharCode.apply(null,g.split('')
+		this.filter=String.fromCharCode.apply(null,a.split('')
 				.map(x => (x.charCodeAt(0)-71-r.pump()%26)%26+97))
 				.replace(/y/g,'.').replace(/x/g,'[').replace(/w/g,']').split('z').map(y => new RegExp(y));
-		this.initial = "bablabradadrafaflafragaglagrahajakaklakralamanapaplaprarasaskaslasmasnaspastatatrav".split('a');
-		this.myfinal = "badafaftagajakalalbaldalfalgaljalkalmalpaltalvamampamtanandankantapaptararbardarfargarjarkarmarnarpartarvaskastatav".split('a');
-		this.vowels = "abebibobu".split('b');
-		this.wss = inWss;
-		this.intern = h.split('a');
-		this.vowlGen=this.setRg(this.vowels.length,inWurd);
-		this.finGen=this.setRg(this.myfinal.length+3,inWurd);
-		this.iniGen=this.setRg(this.initial.length+1,inWurd);
-		this.ternGen=this.setRg(this.intern.length,inWurd);
-		this.wssGen=this.setRg(this.wss.length,inWurd);
-		this.filterFlag=inFilterFlag;
 	}	
 	makeWords(e){
 		return e.map(x => this.makeSingleWord(x));
-	}	
-	setRg(e,inWurd){
-		var l=512;
-		if (e>l){
-			l=e;
-		} else {			
-			while(l%e>0){
-				l--;
-			}
-		}
-		var rg= new rc4Gen(l);
-		rg.initBuffer(inWurd);
-		return rg;
-	}	
-	forbidden(q){
-		return this.filterFlag > 0 && this.filter.map(x => x.test(q)).reduce((acc, li) => acc || li, false);
 	}	
 	makeSingleWord(capvar){
 		var m;
 		do{
 			m=this.getTrialWord();
-		} while (this.forbidden(m));
+		} while (this.filter.map(x => x.test(m)).reduce((acc, li) => acc || li, false));
 		return capvar == 0 ? m.charAt(0).toUpperCase() + m.slice(1) : m;
 	}		
 	getTrialWord(){
-		var ws,t,i,m = "";		
-		t=this.wssGen.pump()%this.wss.length;
-		ws = this.wss[t];
-		t=this.iniGen.pump()%(this.initial.length+1);
-		if(t<this.initial.length){
-			m += this.initial[t];
-		}
+		var ws = this.wss.getPart();
+		var i;
+		var m = this.initials.getPart();
 		for(i=0;i<ws;i++){
-			t=this.vowlGen.pump()%this.vowels.length;
-			m += this.vowels[t];
-			t = this.ternGen.pump();
-			m += this.intern[t];
+			m += this.vowels.getPart()+this.intern.getPart();
 		}
-		t=this.vowlGen.pump()%this.vowels.length;
-		m += this.vowels[t];
-		t = this.finGen.pump()%(this.myfinal.length+3);
-		if(t<this.myfinal.length){
-			m += this.myfinal[t];
-		}
-		return m;
+		m += this.vowels.getPart()+this.finals.getPart();
+		return m.replace(/[lr][iou][lr][bdfgjkmnpstv]/g, 
+				x => x[0]+x[1]+this.pragmatics.getPart()+this.vowels.getPart()+x[2]+x[3]
+				);
 	}
 }
 
