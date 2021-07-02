203ac7f4e5e4de5fa48f8bbb23d908fc41293eb1
Chris Weaver <chrweave@gmail.com>
2021-07-01 22:33:04 -0500

    newwkwt

---
+++ kwt/main.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct _al{
+    void* data;
+    struct _al * next;
+};
+typedef struct _al AbstractList;
+
+struct _at{
+    void* data;
+    struct _at * left;
+    struct _at * right;
+};
+typedef struct _at AbstractTree;
+
+struct _av{
+    void * data;
+    void * current;
+    int size;
+};
+
+typedef struct _av AbstractVector;
+
+struct _ep{
+    void ** pointerPool;
+    void * data;
+    int datumSize;
+    int poolSize;
+    int current;
+};
+
+typedef struct _ep ExpandingPool;
+
+
+void readFileList(char * fname);
+long * getFilePointersForStartOfFileNames(FILE* f);
+void setTreeChild(AbstractTree * parent, AbstractTree * child, int selector);
+void flushList(AbstractList * a);
+ExpandingPool * getExpandingPool(int initialSize, int inDatumSize);
+
+
+ExpandingPool * getExpandingPool(int initialSize, int inDatumSize){
+    ExpandingPool * ep =(ExpandingPool*)malloc(sizeof(ExpandingPool));
+    int i = 0;
+    char * harvester = NULL;
+    ep->datumSize=inDatumSize;
+    ep->poolSize=initialSize;
+    ep->current = 0;
+    ep->data=malloc(initialSize*inDatumSize);
+    ep->pointerPool=malloc(sizeof(void*)*initialSize);
+    harvester=(char*)ep->data;
+    for(i=0;i<initialSize;i++){
+        ep->pointerPool[i]=harvester;
+        harvester+=ep->datumSize;
+    }
+    return ep;
+}
+
+
+void flushList(AbstractList * a){
+    AbstractList * t=a;
+    AbstractList *o=a;
+    while (t!=NULL){
+        if(t->data!=NULL){
+            free(t->data);
+        }
+        o=t;
+        t=t->next;
+        free(o);
+    }
+}
+
+
+void setTreeChild(AbstractTree * parent, AbstractTree * child, int selector){
+    if (selector){
+        parent->left=child;
+    } else {
+        parent->right=child;
+    }
+}
+
+
+
+long * getFilePointersForStartOfFileNames(FILE* f){
+    long * ret=NULL;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    long p = ftell(f);
+
+
+    while ((nread = getline(&line, &len, f)) != -1) {
+        printf("%ld\n", p);
+        p = ftell(f);
+        printf("%s@@@",line);
+    }
+    return ret;
+}
+
+void readFileList(char * fname){
+    FILE * f=NULL;
+    f=fopen(fname,"rb");
+    if(f!=NULL){
+        getFilePointersForStartOfFileNames(f);
+    }
+}
+
+int main(int argc, char ** argv)
+{
+    if(argc>1){
+        readFileList(argv[1]);
+    }
+}
