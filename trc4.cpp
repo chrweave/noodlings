96168dbe3d08ca40efb6bc44de5c90b78fcbce8a
chrweave <53875531+chrweave@users.noreply.github.com>
2022-09-20 14:18:36 -0500

    Update trc4.cpp

--- trc4.cpp
+++ trc4.cpp
@@ -4,11 +4,17 @@
 
 int main (int argc,char ** argv){
   if(argc>3){
-    Rc4gen * rc = new Rc4gen(atoi (argv[1]),argv[2]);
+    int v= atoi (argv[1])
+    Rc4gen * rc = new Rc4gen(v,argv[2]);
     int i = 0;
     int l=atoi(argv[3]);
+    int * pr =rc->getPerm();
     for(i=0;i<l;i++){
       printf("%d ",rc->getNext());
+    }
+    printf("\n");
+    for(i=0;i<v;i++){
+      printf("%d",pr[i]%2);
     }
     printf("\n");
   }
