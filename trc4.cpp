827da62f73092de9f63a82f7c032ead3ceb15939
chrweave <53875531+chrweave@users.noreply.github.com>
2022-09-20 13:33:47 -0500

    Create trc4.cpp

---
+++ trc4.cpp
@@ -0,0 +1,15 @@
+#include<cstdio>
+#include<ctdlib>
+#include "Rc4gen.h"
+
+int main (int argc,char ** argv){
+  if(argc>3){
+    Rc4gen * rc = new Rc4gen(atoi (argv[1]),argv[2]);
+    int i = 0;
+    int l=atoi(argv[3]);
+    for(i=0;i<l;i++){
+      printf("%d ",rc->getNext());
+    }
+    printf("\n");
+  }
+}
