06ecd1df1a7873dcacb56cccc62992c48f361c32
chrweave <53875531+chrweave@users.noreply.github.com>
2022-09-23 11:24:28 -0500

    Update randtree.cpp

--- randtree.cpp
+++ randtree.cpp
@@ -1,58 +1,77 @@
 #include<cstdio>
+
 #include<cstdlib>
+
 #include "rc4gen.h"
 
 class RandTree {
-public:
-  RandTree();
+    public:
+        RandTree();
     ~RandTree();
-  void getTree(int size, char * seed);
-private:
-  void flip(int * in , int * out, int size);
-  void setVecsFromRc4gen();
-  void clearVecs();
-  int * vecs[2];
-  Rc4gen * rc;
+    void getTree(int size, char * seed);
+    private:
+        void flip(int * in , int * out, int size);
+    void setVecsFromRc4gen();
+    void clearVecs();
+    int vecSize;
+    int * vecs[2];
+    Rc4gen * rc;
 };
 
 RandTree::RandTree() {
-  rc = vecs[0] = vecs[1] = NULL;
+    rc = vecs[0] = vecs[1] = NULL;
+    vecSize = 0;
 }
 void RandTree::clearVecs() {
-  int i = 0;
-  for (i = 0; i < 2; i++) {
-    if (vecs[i] != NULL) {
-      delete[] vecs[i];
-      vecs[i] = NULL;
+    int i = 0;
+    for (i = 0; i < 2; i++) {
+        if (vecs[i] != NULL) {
+            delete[] vecs[i];
+            vecs[i] = NULL;
+        }
     }
-  }
 }
 
 RandTree::~RandTree() {
-  clearVecs();
+    clearVecs();
 }
 
 void RandTree::flip(int * in , int * out, int size) {
-  int i = 0;
-  for (i = 0; i < size; i++) {
-    out[i] = in [i] * (-1);
-  }
+    int i = 0;
+    for (i = 0; i < size; i++) {
+        out[i] = in [i] * (-1);
+    }
 }
-RandTree::void setVecsFromRc4gen(Rc4gen * rc){
+
+int RandTree::setVecsFromRc4gen() {
+    int ret = 0;
+    int i = 0;
+    int * p = rc -> getPerm(); /* allocated by rc->getPerm() */
+    if (vecSize > 0) {
+        ret = 1;
+        for (i = 0; i < vecSize; i++) {
+            vecs[0][i] = (p[i] % 2) * 2 - 1;
+        }
+    }
+    delete[] p;
+    return ret;
 }
-void RandTree::getTree(int size, char * seed) {  
-  int i;
-  int vtest = 1;
-  if(rc ==NULL){
-    rc = new Rc4gen(size, seed);
-  } else
-    rc->setSate(size,seed);
-  }
-  for (i = 0; i < 2; i++) {
-    vecs[i] = new int[size];
-    vtest=vtest&&(vecs[i] !=NULL);
-  }
-  if(vtest){
-    setVecsFromRc4gen();
-  }
+
+void RandTree::getTree(int size, char * seed) {
+    int i;
+    int vtest = 1;
+    if (rc == NULL) {
+        rc = new Rc4gen(size, seed);
+        vecSize = size;
+    } else {
+        rc -> setSate(size, seed);
+        vecSize = size;
+    }
+    for (i = 0; i < 2; i++) {
+        vecs[i] = new int[size];
+        vtest = vtest && (vecs[i] != NULL);
+    }
+    if (vtest) {
+        setVecsFromRc4gen();
+    }
 }
