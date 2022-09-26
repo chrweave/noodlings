d57014f655c1dc0a57d34a5aa03c50ebd4a848e2
chrweave <53875531+chrweave@users.noreply.github.com>
2022-09-26 09:02:31 -0500

    indent

--- rc4gen.h
+++ rc4gen.h
@@ -1,20 +1,19 @@
 #ifndef RC4GEN_H
 #define RC4GEN_H
-class Rc4gen
-{
-public:
-  Rc4gen ();
-  Rc4gen (int size, char *seed);
-   ~Rc4gen ();
-  int getNext ();
-  int *getPerm ();
-  void setState(int size, char *seed);
-private:
-  int l;
-  int x;
-  int y;
-  int t;
-  int *a;
-  void pump ();
+class Rc4gen {
+    public:
+        Rc4gen();
+    Rc4gen(int size, char * seed);
+    ~Rc4gen();
+    int getNext();
+    int * getPerm();
+    void setState(int size, char * seed);
+    private:
+        int l;
+    int x;
+    int y;
+    int t;
+    int * a;
+    void pump();
 };
 #endif
