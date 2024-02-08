1e35a6797a6f40d4d52e85e73e6ade60a6076a93
chrweave <53875531+chrweave@users.noreply.github.com>
2024-02-08 08:52:50 -0600

    binom64

--- rhashf.c
+++ rhashf.c
@@ -15,6 +15,18 @@ double chiSquaredStatistic(double * observed, double * expected, int length){
     return return_value;
 }
 
+void binom64(double * dist){
+    int i = 0;
+    double d = 0.0;
+    double g = lgamma(65);
+    for(i=0;i<65;i++){
+        d = g;
+        d -= lgamma((double)(i+1))
+        d -= lgamma((double)(65-i));
+        dist[i]=exp(d);
+    }
+}
+
 int main(int argc, char ** argv){
     return 0;
 }
