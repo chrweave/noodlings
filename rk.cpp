51c2592e4eb16f8f515574b3cbbffe62d9a216eb
chrweave <53875531+chrweave@users.noreply.github.com>
2025-02-19 09:44:26 -0600

    on box

--- rk.cpp
+++ rk.cpp
@@ -1,3 +1,23 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdint.h>
+#include"rk.h"
+
+int main(int argc, char** argv){
+    return 0;
+}
+
+template<typename T> Arena<T>::~Arena<T>(){
+    Arena<T> * o;
+    for (Arena<T> * t=next; t!=NULL;o=t, t=t->next){
+        o->next=NULL;
+        delete o;
+    }
+}
+
+template<typename T> T* Arena<T>::fetch(Arena<T> & a){
+    T * ret=&supply[index];
+    index ++;
+    if(index==MEG){
+        a->next=new Arena<T>();
+        a=a->next;
+    }
+    return ret;
+}
